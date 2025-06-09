05b2b2a3ec52bfb7d5b21a64c723948538e412e0
juulenpeerez <124788538+juulenpeerez@users.noreply.github.com>
2025-06-10 00:37:25 +0200

    Delete uah_photsat_freertos directory

--- uah_photsat_freertos/Debug/service_libraries/crc/src/crc.d
+++
@@ -1,7 +0,0 @@
-service_libraries/crc/src/crc.o: ../service_libraries/crc/src/crc.c \
- /home/atcsol/git/Trabajo/uah_photsat_freertos/service_libraries/crc/include/public/crc.h \
- /home/atcsol/git/Trabajo/uah_photsat_freertos/llsw/config/include/public/basic_types.h
-
-/home/atcsol/git/Trabajo/uah_photsat_freertos/service_libraries/crc/include/public/crc.h:
-
-/home/atcsol/git/Trabajo/uah_photsat_freertos/llsw/config/include/public/basic_types.h:
--- uah_photsat_freertos/Debug/service_libraries/edroomsl/edroombp/src/freertossapi/timespec.d
+++
@@ -1,8 +0,0 @@
-service_libraries/edroomsl/edroombp/src/freertossapi/timespec.o: \
- ../service_libraries/edroomsl/edroombp/src/freertossapi/timespec.c \
- ../service_libraries/edroomsl/edroombp/src/freertossapi/../../../edroombp/include/public/timespec.h \
- /home/atcsol/git/Trabajo/uah_photsat_freertos/llsw/config/include/public/basic_types.h
-
-../service_libraries/edroomsl/edroombp/src/freertossapi/../../../edroombp/include/public/timespec.h:
-
-/home/atcsol/git/Trabajo/uah_photsat_freertos/llsw/config/include/public/basic_types.h:
